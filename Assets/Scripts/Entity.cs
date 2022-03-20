5fed3e4db1e4e8fc1ca792778cabd73cae8b7a66
abettis56 <alanbettis56@gmail.com>
2022-03-20 18:14:33 -0400

    Implemented axis-locking movement animation!

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -17,10 +17,10 @@ namespace Prometheus
         public float MoveSpeed { get => _moveSpeed; set => _moveSpeed = value; }
         protected bool _canMove;
         public bool CanMove { get => _canMove; set => _canMove = value; }
-        protected Vector2 _pastVelocity;
         protected Rigidbody2D _physicsBody;
         public Rigidbody2D PhysicsBody { get => _physicsBody; }
         protected AnimFSM _animFSM;
+        protected bool _isXAxisDominant;
 
         public Entity(float moveSpeed,
                       bool canMove,
@@ -32,47 +32,81 @@ namespace Prometheus
             this._physicsBody = physicsBody;
             this._animFSM = new AnimFSM(animator);
 
-            this._pastVelocity = Vector2.zero;
+            this._isXAxisDominant = true;
         }
 
         public void Move(int x, int y)
         {
-            if(!this._canMove) { return; }
+            if (!this._canMove) { return; }
             this._physicsBody.velocity = new Vector2((float)x, (float)y).normalized * this._moveSpeed;
-            
-            //Animation state
-            switch(x)
-            {
-                case -1:
-                    this._animFSM.StateTransition(AnimationStates.left.ToString());
-                    break;
-                case 0:
-                    switch(y)
-                    {
-                        case -1:
-                            this._animFSM.StateTransition(AnimationStates.down.ToString());
-                            break;
-                        case 0:
-                            break;
-                        case 1:
-                            this._animFSM.StateTransition(AnimationStates.up.ToString());
-                            break;
-                    }
-                    break;
-                case 1:
-                    this._animFSM.StateTransition(AnimationStates.right.ToString());
-                    break;
-            }
+
+            SetAnimationState(x, y);
 
             //Animation checks 
             float vecLength = this._physicsBody.velocity.sqrMagnitude;
-            if(x == 0 && y == 0)
+            if (x == 0 && y == 0)
             {
                 this._animFSM.PlayAnim("idle");
             }
             else
             {
                 this._animFSM.PlayAnim("move");
+            }
+        }
+
+        public void SetAnimationState(int x, int y)
+        {
+            if(this._isXAxisDominant)
+            {
+                switch (x)
+                {
+                    case -1:
+                        this._animFSM.StateTransition(AnimationStates.left.ToString());
+                        break;
+                    case 0:
+                        this._isXAxisDominant = false;
+                        switch (y)
+                        {
+                            case -1:
+                                this._animFSM.StateTransition(AnimationStates.down.ToString());
+                                break;
+                            case 0:
+                                break;
+                            case 1:
+                                this._animFSM.StateTransition(AnimationStates.up.ToString());
+                                break;
+                        }
+                        break;
+                    case 1:
+                        this._animFSM.StateTransition(AnimationStates.right.ToString());
+                        break;
+                }
+            }
+            else
+            {
+                switch (y)
+                {
+                    case -1:
+                        this._animFSM.StateTransition(AnimationStates.down.ToString());
+                        break;
+                    case 0:
+                        this._isXAxisDominant = true;
+                        switch (x)
+                        {
+                            case -1:
+                                this._animFSM.StateTransition(AnimationStates.left.ToString());
+                                break;
+                            case 0:
+                                break;
+                            case 1:
+                                this._animFSM.StateTransition(AnimationStates.right.ToString());
+                                break;
+                        }
+                        break;
+                    case 1:
+                        this._animFSM.StateTransition(AnimationStates.up.ToString());
+                        break;
+                }
             }
         }
     }
